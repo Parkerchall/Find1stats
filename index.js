eb5184830d83445aabf75658b6d17f73aa7d588b
parkerchall@gmail.com <parkerchall@Parkers-MacBook-Air.local>
2021-06-11 11:26:46 -0400

    added search bar (non functional)

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const getDriver = () => {
-  const url = "http://ergast.com/api/f1/drivers";
+  const url = "http://ergast.com/api/f1/drivers.json?limit=100";
   
   axios 
   .get(url)
   .then((res) => console.log(res.data))
   .catch((error) => console.error(error));
 }
-
 getDriver();
+const searchBar = document.getElementById('searchBar')
+  searchBar.addEventListener('keyup', (e) => {
+    console.log(e.target.value);
+  })
