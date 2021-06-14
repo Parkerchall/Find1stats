e5e28ddab59584ea13c8d0f6e30cea2475cea14b
parkerchall@gmail.com <parkerchall@Parkers-MacBook-Air.local>
2021-06-14 02:18:57 -0400

    added card, better search engine, and more css, justify content is still broken and also need to correct currentDriver function on line 43 in JS

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const searchBoxDropdown = document.querySelector(".search-box-dropdown");
+const searchButton = document.querySelector("#search")
+const jubran = document.querySelector("#searchBar");
+
+const userInput = document.getElementById(searchBar)
 function clearSearchBoxDropdown() {
   while(searchBoxDropdown.lastChild){
     searchBoxDropdown.removeChild(searchBoxDropdown.lastChild)
   }
 }
 const getDriver = async () => {
-  const url = "http://ergast.com/api/f1/drivers.json?limit=1000";
+  const url = `http://ergast.com/api/f1/2021/drivers.json?`;
   
   const response = await axios 
   .get(url)
@@ -25,14 +29,33 @@ const getDriver = async () => {
         || (driver.permanentNumber && driver.permanentNumber.includes(e.target.value))
         || driver.dateOfBirth.includes(e.target.value)
       )}));
-      
-      //search results
-      console.log(filteredDrivers);
-      filteredDrivers.slice(0,16).forEach(driver => {
-       const p = document.createElement("p");
-       p.innerText = driver.givenName + " " + driver.familyName + " " +driver.dateOfBirth + " " + driver.nationality;
-        searchBoxDropdown.appendChild(p);
+        
+        //search results
+        filteredDrivers.slice(0,16).forEach(driver => {
+          const p = document.createElement("p");
+          p.innerText = driver.givenName + " " + driver.familyName + " " +driver.dateOfBirth + " " + driver.nationality;
+          searchBoxDropdown.appendChild(p);
+        })
       })
-  })
-}
-getDriver();
+    }
+    getDriver();
+    const getCurrentDriver = async () => {
+      const currentDriver = jubran.value.toLowerCase();
+      console.log(jubran.value);
+      const response = await axios.get(`http://ergast.com/api/f1/2021/drivers/${currentDriver}.json?`)
+      const driverDetails = response.data.MRData.DriverTable.Drivers;
+      displayDriver(driverDetails);
+    };
+
+    const displayDriver = (driverDetails) => {
+      const displayDetails= document.querySelector('.driver');
+      const displayName = document.createElement('displayName');
+      displayName.innerText = driverDetails.givenName + " "  + driverDetails.permanentNumber + " " + driverDetails.dateOfBirth + " " + driverDetails.nationality; 
+      displayDetails.appendChild(displayName)
+    }
+    jubran.addEventListener('change', displayDriver)
+
+    // searchButton.addEventListener("click", (event) => {
+    //   event.preventDefault();
+    //   getData();
+    // })
